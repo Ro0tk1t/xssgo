9e39aba7904b482cd94f5db770ae40066c24424c
rootkit <rootkit@localhost.localdomain>
2019-01-07 15:39:59 +0800

    add cli

---
+++ xssgo/cli.go
@@ -0,0 +1,38 @@
+package cli
+
+import (
+	"flag"
+	"fmt"
+)
+
+
+type Parse struct{
+    url string
+    data string
+    file string
+    spider bool
+}
+
+func cli(){
+	var (
+		url string
+		data string
+		file string
+        spider bool
+	)
+
+	flag.StringVar(&url, "u", "http://example.com", "Target URL")
+	flag.StringVar(&data, "d", "", "POST data")
+	flag.StringVar(&file, "f", "", "fuzz keyword file")
+    flag.BoolVar(&spider, "s", false, 'use spider')
+	flag.Parse()
+
+    Parsed := &Parse
+    Parsed.url = url
+    Parsed.data = data
+    Parsed.file = file
+    Parsed.spider = spider
+
+    return Parsed
+}
+
