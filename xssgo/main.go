fdbe3a31a7e1a14755f1a3525ba28e2935353493
rootkit <1576235794@qq.com>
2019-02-08 23:46:50 +0800

    update main

--- xssgo/main.go
+++ xssgo/main.go
@@ -3,29 +3,44 @@ package main
 import (
     "cli"
     "fmt"
+    "net/url"
     "request"
 )
 
 type Result struct{
-    page string
-    param string
-    payload string
-    weakness bool
+    Page string
+    Param string
+    Payload string
+    Weakness bool
 }
 
 type Conf struct{
-    input cli.Parse
-    url string
-    dict string
-    spider bool
-    results *Result
+    Input cli.Parse
+    Scheme string
+    Host string
+    Path string
+    Username string
+    Password string
+    Dict string
+    Results *Result
 }
 
 func main(){
     parsed := cli.Cli()
     conf := Conf{}
-    conf.input = *parsed
-    body, _ := request.HttpDo("GET", conf.input.Url, "", map[string]string{})
+    conf.Input = *parsed
+    u, err := url.Parse(parsed.Url)
+    if err != nil{
+        fmt.Println("[-]  Invalid URL !")
+        return
+    }
+    conf.Scheme = u.Scheme
+    conf.Host = u.Host
+    conf.Path = u.Path
+    conf.Username = u.User.Username()
+    conf.Password, _ = u.User.Password()
+    fmt.Println(u)
+    body, _ := request.HttpDo("GET", conf.Input.Url, "", map[string]string{})
     fmt.Println(body)
     return
 }
